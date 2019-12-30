6651c33354769458ee3d2de3b2a9d3a24ef56f92
liuchg <348437207@qq.com>
2019-12-30 09:07:29 +0800

    day 4

---
+++ src/app/user/pig/pig.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pig',
+  templateUrl: './pig.component.html',
+  styleUrls: ['./pig.component.css']
+})
+export class PigComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
