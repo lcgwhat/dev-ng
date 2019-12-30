5334b9a4540cf0a23831aaada7c830c5a3873f36
lcgwhat <348437207@qq.com>
2019-12-30 21:27:41 +0800

    dd

--- src/app/user/user-routing.module.ts
+++ src/app/user/user-routing.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import {UserComponent} from './user/user.component';
 import {LoginComponent} from './login/login.component';
 import {RegisterComponent} from './register/register.component';
-import {PigComponent} from './pig/pig.component';
 
 
 const routes: Routes = [
@@ -11,7 +10,6 @@ const routes: Routes = [
     path: 'user', component: UserComponent, children: [
       {path: 'login', component: LoginComponent},
       {path: 'register', component: RegisterComponent},
-      {path: 'pig', component: PigComponent}
     ]
   }
 ];
